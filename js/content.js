c12b86f8679f4144a16e0a4733568746f60684ea
Mathieu CAROFF <mathieu.caroff@free.fr>
2024-08-26 21:34:49 +0200

    remove logging statements

--- js/content.js
+++ js/content.js
@@ -1,32 +1,31 @@
 // @ts-check
 
-async function getVideoElement() {
+async function getVideoElement(retryCount = 0) {
+  if (retryCount < 0) {
+    console.warn("YoutubeTabPlaylist: No video found!")
+    throw new Error("No video found!")
+  }
+
   let videoList = document.querySelectorAll("video")
   if (videoList.length === 0) {
-    console.warn("YoutubeTabPlaylist: No video found!")
-    return new Promise((resolve) =>
-      setTimeout(() => resolve(getVideoElement()), 1000)
-    )
+    await new Promise((resolve) => setTimeout(() => resolve(0), 1000))
+    return getVideoElement(retryCount - 1)
   } else {
     if (videoList.length > 1) {
       console.warn("YoutubeTabPlaylist: More than one video element found!")
-    } else {
-      console.warn("YoutubeTabPlaylist: Video found!")
     }
     return videoList[0]
   }
 }
 
-getVideoElement().then((video) => {
+getVideoElement(30).then((video) => {
   video.addEventListener("ended", () => {
-    console.warn("YoutubeTabPlaylist: event: video ended")
     chrome.runtime.sendMessage("video:ended")
   })
 })
 
 chrome.runtime.onMessage.addListener((body, sender, sendResponse) => {
-  console.warn("YoutubeTabPlaylist: got messge:", body)
   if (body === "play") {
-    getVideoElement().then((video) => video.play())
+    getVideoElement(6).then((video) => video.play())
   }
 })
